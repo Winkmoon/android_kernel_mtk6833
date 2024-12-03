cfc7caffb5bcbc8196f4884ad42c21d7ebd6af27
2481849298 <2437586216@qq.com>
2024-12-03 22:29:29 +0800

    666

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -128,6 +128,22 @@ err_ws:
 }
 EXPORT_SYMBOL_GPL(wakeup_source_create);
 
+/**
+ * wakeup_source_drop - Prepare a struct wakeup_source object for destruction.
+ * @ws: Wakeup source to prepare for destruction.
+ *
+ * Callers must ensure that __pm_stay_awake() or __pm_wakeup_event() will never
+ * be run in parallel with this function for the same wakeup source object.
+ */
+void wakeup_source_drop(struct wakeup_source *ws)
+{
+	if (!ws)
+		return;
+
+	__pm_relax(ws);
+}
+EXPORT_SYMBOL_GPL(wakeup_source_drop
+
 /*
  * Record wakeup_source statistics being deleted into a dummy wakeup_source.
  */
@@ -174,7 +190,7 @@ void wakeup_source_destroy(struct wakeup_source *ws)
 	if (!ws)
 		return;
 
-	__pm_relax(ws);
+    wakeup_source_drop(ws);
 	wakeup_source_record(ws);
 	wakeup_source_free(ws);
 }
